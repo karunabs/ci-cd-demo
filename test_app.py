972aefcd58635c08bfd5b0e08f1125209bc7e98b
karunabs <karuna.banavar@gmail.com>
2026-04-04 09:45:48 -0700

    Added a test to verify db connection

--- test_app.py
+++ test_app.py
@@ -11,7 +11,10 @@ def test_add_with_fixtures(numbers):
   assert add(a, b) == 5
 
 ## This test is an example with class implementation
-
 def test_add_func_calculator(calculator):
   result = calculator.add_func(2,3)
   assert result == 5
+
+def test_query_db(db_connection):
+  ## here db connection is valid for the entire session
+  assert db_connection == "Database Connection"
