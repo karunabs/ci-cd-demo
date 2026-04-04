23cae986a0ef98f04465ef9eb24befda200d5af8
karunabs <karuna.banavar@gmail.com>
2026-04-04 09:22:57 -0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,3 +9,9 @@ def test_add_fail():
 def test_add_with_fixtures(numbers):
   a, b = numbers
   assert add(a, b) == 5
+
+## This test is an example with class implementation
+
+def test_add_func_calculator(calculator):
+  result = calculator.add_func(2,3)
+  assert result == 5
