00460d71b25a71b54ad0679678a91db9f4392ecc
karunabs <karuna.banavar@gmail.com>
2026-04-04 09:04:13 -0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,8 +2,8 @@ import pytest
 
 @pytest.fixture()
 def numbers():
-  """
-  this fixture provides test data. 
-  It runs before each test, which uses it. 
-  """
+  
+  ## this fixture provides test data. 
+  ## It runs before each test, which uses it. 
+  
   return 2,3
